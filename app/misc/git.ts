a5ac23d5d02dc47faac4956c6cd7c983d642672a
Jordan Benning <Jordan@Jordans-MacBook-Pro-2.local>
2019-06-14 17:22:39 -0700

    Improve UX and fix stash list items becoming anchors

--- app/misc/git.ts
+++ app/misc/git.ts
@@ -805,7 +805,7 @@ async function deleteTag(tagName, refresh = true) {
           .then((res) =>{
             resolve(res);
           })
-          .catch((err) => console.log(err));      
+          .catch((err) => console.log(err));
       }).then(() => {
         if(refresh)
           refreshAll(repository);
