ca165a40cb285c0c516c389feee0dbb96b40f51c
Carter Renard <carter.renard@gmail.com>
2019-06-16 15:36:11 -0700

    Change authenticate.component for token use

--- app/components/authenticate/authenticate.component.ts
+++ app/components/authenticate/authenticate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, } from "@angular/core";
+import { create } from "domain";
 
 @Component({
   selector: "user-auth",
@@ -7,44 +8,22 @@ import { Component, OnInit, } from "@angular/core";
 
 export class AuthenticateComponent implements OnInit {
   ngOnInit(): any {
-    // useSavedCredentials returns true if there is a saved credential and uses it.
-    if (useSavedCredentials()){
-      // @ts-ignore
-      document.getElementById("rememberLogin").checked = true;
-    } else {
-      // @ts-ignore
-      document.getElementById("rememberLogin").checked = false;
-    }
   }
 
   switchToMainPanel(): void {
     console.log("switchToMainPanel: Authenticate.component.ts");
-    // Check if either the password/username or both fields are empty and show an appropriate message if they are
-    if (document.getElementById('password').value == "" && document.getElementById('username').value == "") {
-      emptyPassword();
-      emptyUsername();
-    } else if (document.getElementById('password').value == "") {
-      emptyPassword();
-      notEmptyUsername();
-    } else if (document.getElementById('username').value == "") {
-      emptyUsername();
-      notEmptyPassword();
-    } else {
-      // Both the fields filled so check if they can log in
-      notEmptyPassword();
-      notEmptyUsername();
-      
-      document.getElementById('grey-out').style.display = 'block';
+
+    //If there is a token then sign in
+    let file = 'data.json';
+    // check if the data.json file exists
+    if (fs.existsSync(file)) {
       getUserInfo(switchToAddRepositoryPanel);
     }
-  }
-
-  createNewAccount(): void {
-    window.open("https://github.com/join?", "_blank");
-  }
-  
-  openGitHubPasswordResetPage() : void {
-    window.open("https://github.com/password_reset", "_blank");
+    //Otherwise create token then sign in
+    else {
+      createOauthToken();
+      getUserInfo(switchToAddRepositoryPanel);
+    }
   }
 }
 
@@ -57,44 +36,4 @@ function createOauthToken() {
   //openOauthWindow();
   //For now and testing just enter a valid token and store it
   storeOauthToken('PutYourTokenHere');
-}
-
-/*
-  If the password is empty, made the input field for the password red and show a message saying its 
-  a required field.
-*/
-function emptyPassword() {
-  document.getElementById('password').classList.add("error");
-  document.getElementById('password-error').style.display = 'inline-block';
-}
-
-/*
-  If the username is empty, made the input field for the username red and show a message saying its 
-  a required field.
-*/
-function emptyUsername() {
-  document.getElementById('username').classList.add('error');
-  document.getElementById('username-error').style.display = 'inline-block';
-}
-
-/*
-  If the username is not empty and the input field has been made red, change it back to normal and get
-  rid of the warning message.
-*/
-function notEmptyUsername() {
-  if (document.getElementById('username').classList.contains('error')) {
-    document.getElementById('username').classList.remove('error');
-    document.getElementById('username-error').style.display = 'none';
-  }
-}
-
-/*
-  If the password is not empty and the input field has been made red, change it back to normal and get
-  rid of the warning message.
-*/
-function notEmptyPassword() {
-  if (document.getElementById('password').classList.contains('error')) {
-    document.getElementById('password').classList.remove('error');
-    document.getElementById('password-error').style.display = 'none';
-  }
 }
