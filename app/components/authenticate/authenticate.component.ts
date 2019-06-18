1fff6a48431d1a1285e2b17720e5461e1db94c65
Carter Renard <carter.renard@gmail.com>
2019-06-18 15:38:44 -0700

    Deal with errors when access tokens are invalid

--- app/components/authenticate/authenticate.component.ts
+++ app/components/authenticate/authenticate.component.ts
@@ -16,6 +16,7 @@ export class AuthenticateComponent implements OnInit {
     //If there is a token then sign in
     let file = 'token.json';
     // check if the data.json file exists
+    // logic for invalid token in authenticate.ts, existsSync returns true or false if path exists
     if (fs.existsSync(file)) {
       getUserInfo(switchToAddRepositoryPanel);
     }
